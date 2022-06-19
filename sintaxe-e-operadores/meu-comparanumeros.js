b7ca1bfaed4b8103c9cbcbc73c565eb28ad14eca
leovilaeu <leovilamail@gmail.com>
2022-06-19 15:22:55 -0300

    Exercicio compara números.

---
+++ sintaxe-e-operadores/meu-comparanumeros.js
@@ -0,0 +1,13 @@
+function comparaNumeros(num1, num2) {
+    const saoIguais = num1 === num2;
+    const soma = num1 + num2;
+/* Função if padrão
+    if(saoIguais) {
+        return "São iguais";
+    }
+    return "Não são iguais"; */
+
+/* Função if ternário
+    return saoIguais ? "São iguais" : "Não são iguais"
+    */     
+}
